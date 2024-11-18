3fe92327d3302674c14b10e2272dadc1e28a986f
maleja1192 <alejis2425@hotmail.com>
2024-11-18 09:02:15 -0500

    se añade actitivy ingreso lote

---
+++ app/src/main/java/com/example/webf1movil1704/Activity_ingreso_lotes.java
@@ -0,0 +1,24 @@
+package com.example.webf1movil1704;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_ingreso_lotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ingreso_lotes);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
