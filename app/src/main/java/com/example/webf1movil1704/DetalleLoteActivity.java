2a9b0602dc019f940bce8eea1c43e31471809cd0
Moi~Rouhs <carsrouhs@gmail.com>
2024-11-23 00:08:50 -0500

    Agregado metodo eliminar

--- app/src/main/java/com/example/webf1movil1704/DetalleLoteActivity.java
+++ app/src/main/java/com/example/webf1movil1704/DetalleLoteActivity.java
@@ -3,7 +3,10 @@ package com.example.webf1movil1704;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,7 @@ import com.example.webf1movil1704.DB.DbHelper;
 
 public class DetalleLoteActivity extends AppCompatActivity {
 
+    private Button btnEliminar;
     private TextView textViewDetalle;
 
     @Override
@@ -24,6 +28,18 @@ public class DetalleLoteActivity extends AppCompatActivity {
         if (loteId != -1) {
             cargarDetalleLote(loteId);
         }
+
+        btnEliminar = findViewById(R.id.buttonEliminar);
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eliminarLote(loteId);
+                Intent intent = new Intent(DetalleLoteActivity.this, ListarLotesActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        });
+
     }
 
     private void cargarDetalleLote(int id) {
@@ -42,5 +58,12 @@ public class DetalleLoteActivity extends AppCompatActivity {
             textViewDetalle.setText(detalle);
         }
         cursor.close();
+    }
+
+    private void eliminarLote(int id) {
+        DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete(DbHelper.TABLE_NAME, "id = ?", new String[]{String.valueOf(id)});
+        db.close();
     }
 }
